73bc3ab3d47dcecf65a556e7808bc9a6af596f14
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-02-02 13:19:22 +0000

    Released version 1.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "pagebrowse"
+# Extension Manager/Repository config file for ext "pagebrowse".
 #
-# Auto generated 11-11-2008 15:52
+# Auto generated 02-02-2010 15:18
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.1',
+	'version' => '1.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.0.0-4.3.99',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"c1e2";s:12:"ext_icon.gif";s:4:"c538";s:17:"ext_localconf.php";s:4:"821e";s:14:"ext_tables.php";s:4:"cccc";s:14:"doc/manual.sxw";s:4:"512a";s:30:"doc/template-structure.graffle";s:4:"e24e";s:31:"pi1/class.tx_pagebrowse_pi1.php";s:4:"cf24";s:19:"pi1/flexform_ds.xml";s:4:"b1b9";s:17:"pi1/locallang.xml";s:4:"e8ee";s:14:"res/styles.css";s:4:"2bbb";s:17:"res/template.html";s:4:"23c1";s:33:"static/page_browser/constants.txt";s:4:"156c";s:29:"static/page_browser/setup.txt";s:4:"6aa6";}',
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"7b91";s:12:"ext_icon.gif";s:4:"c538";s:17:"ext_localconf.php";s:4:"821e";s:14:"ext_tables.php";s:4:"cccc";s:14:"doc/manual.sxw";s:4:"b372";s:30:"doc/template-structure.graffle";s:4:"e24e";s:31:"pi1/class.tx_pagebrowse_pi1.php";s:4:"0d5c";s:19:"pi1/flexform_ds.xml";s:4:"b1b9";s:17:"pi1/locallang.xml";s:4:"e8ee";s:14:"res/styles.css";s:4:"34c0";s:18:"res/styles_min.css";s:4:"e907";s:17:"res/template.html";s:4:"d59b";s:33:"static/page_browser/constants.txt";s:4:"156c";s:29:"static/page_browser/setup.txt";s:4:"6aa6";}',
 	'suggests' => array(
 	),
 );
