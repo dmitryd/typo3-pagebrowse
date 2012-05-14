d3d5eeb5cce4040ffb6bca20496ddece625666f3
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-14 07:33:18 +0000

    Fixed bug #6351: Plugin element displays 'Data Structure ERROR'

--- ext_tables.php
+++ ext_tables.php
@@ -8,6 +8,6 @@ t3lib_extMgm::addStaticFile($_EXTKEY,'static/page_browser/', 'Page browser');
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,select_key,pages';
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
 t3lib_extMgm::addPlugin(array('LLL:EXT:pagebrowse/pi1/locallang.xml:tt_content.list_type_pi1', $_EXTKEY . '_pi1'), 'list_type');
-t3lib_extMgm::addPiFlexFormValue($_EXTKEY .'_pi1', 'LLL:EXT:pagebrowse/pi1/flexform_ds.xml');
+t3lib_extMgm::addPiFlexFormValue($_EXTKEY .'_pi1', 'FILE:EXT:pagebrowse/pi1/flexform_ds.xml');
 
 ?>
